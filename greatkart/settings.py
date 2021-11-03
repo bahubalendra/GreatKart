ed36fc117dbad72f484b51e7e8036cd7484eb827
Bahubalendra Barik <60384737+bahubalendra@users.noreply.github.com>
2021-11-03 09:51:29 +0530

    last  update

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -114,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -146,6 +148,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'                                       
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bahubalendra.barik@gmail.com'
-EMAIL_HOST_PASSWORD = 'shreenu@143'
-DEFAULT_FROM_EMAIL = '<bahubalendra.barik>'
+EMAIL_HOST_USER = 'projectpractice418@gmail.com'
+EMAIL_HOST_PASSWORD = 'Shreesa@143'
+DEFAULT_FROM_EMAIL = '<support.greatkart@gmail.com>'
